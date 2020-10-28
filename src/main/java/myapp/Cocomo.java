a0a80149cfae3589c1fecad00411926a07af59f3
Eliseo Garcia <zeog14@gmail.com>
2020-10-27 20:35:10 -0500

    adding all files

--- src/main/java/myapp/Cocomo.java
+++ src/main/java/myapp/Cocomo.java
@@ -16,104 +16,100 @@
 
 package myapp;
 
-import java.lang.*;
 import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.lang.Math;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
-public class Cocomo extends HttpServlet{
+public class Cocomo extends HttpServlet {
 
-	public static  int mo;
-        public static double ef;
-	public static double ti;
-	public static String mode[]={"Organic","Semi-Detached","Embedded"};
-	public static  double[][] table = {{2.4,1.05,2.5,0.38}, {3,1.12,2.5,0.35},{3.6,1.2,2.5,0.32}};	 
-		
-
+  public static int mo;
+  public static double ef;
+  public static double ti;
+  public static String[] mode = {"Organic", "Semi-Detached", "Embedded"};
+  public static double[][] table = {{2.4,1.05,2.5,0.38},{3,1.12,2.5,0.35},{3.6,1.2,2.5,0.32}};
 
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-	String size  = req.getParameter("size");
-	
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-	try{
-	String mode  = modeStr(Integer.parseInt(size));
-	String effort = effortStr(Integer.parseInt(size));
-	String time = timeStr();
-
-	req.setAttribute("mode", mode);
-	resp.setHeader("MODE",mode);
-
-	
-	req.setAttribute("effort", effort);
-	resp.setHeader("EFFORT",effort);
+    String size = req.getParameter("size");
 
 
-	req.setAttribute("time", time);
-	resp.setHeader("TIME",time);
+    try {
+      String mode = modeStr(Integer.parseInt(size));
+      String effort = effortStr(Integer.parseInt(size));
+      String time = timeStr();
+
+      req.setAttribute("mode", mode);
+      resp.setHeader("MODE", mode);
 
 
+      req.setAttribute("effort", effort);
+      resp.setHeader("EFFORT", effort);
 
 
-	RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
-	dispatcher.forward(req,resp);
-	}catch (Exception e){
-		resp.sendRedirect("index.jsp");
-	}
-
-	}
+      req.setAttribute("time", time);
+      resp.setHeader("TIME", time);
 
 
-	private String modeStr(int size){
-	
-		int n = 3;
-	    	double time,staff;
-	        int model = 0;
+      RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+      dispatcher.forward(req, resp);
+    } catch (Exception e) {
+      resp.sendRedirect("index.jsp");
+    }
 
-		//Check the mode according to size
-		if(size >=2 && size <= 50) model =0; // organic
-		else if(size> 50 && size <= 300) model = 1; // semi-detached
-		else if(size>300) model =2; //embedded
-		Cocomo.mo = model;
+  }
 
-		String fin = "The mode is " + mode[model] + System.lineSeparator();
-		
-		return fin;
 
-	}
-	
-	private String effortStr(int size){
-		double effort = table[mo][0] * Math.pow((double)size, table[mo][1]);
-		Cocomo.ef = effort;
-		String fin = "Effort = " + Integer.toString(dround(effort)) + " Person-Month"  + System.lineSeparator();
-		return fin;
+  private String modeStr(int size) {
+    int model = 0;
 
-	}
+      //Check the mode according to size
+      // 0 = organic
 
-	private String timeStr(){
-		double time = table[mo][2] * Math.pow(ef, table[mo][3]);
-		Cocomo.ti = time;
-		String fin = "Development Time = " + Integer.toString(dround(time)) + " Months" + System.lineSeparator();
-		return fin;
-	}
+    if (size > 50 && size <= 300) {
+      model = 1;
+    } // semi-detached
 
-	private String staffStr(){
-		double staff = ef/ti;
+    if (size > 300) {
+      model = 2;
+    } //embedded
 
-		String fin = "Average Staff Required = "+ Integer.toString(fround(staff)) +" Persons" + System.lineSeparator();
-		return fin;
-	}
-	public int fround(double x){
-		return (int)Math.ceil(x);
-	}
+    Cocomo.mo = model;
 
-	public int dround(double y){
-		return (int)(Math.round(y * 100000.00) / 100000.00);
-	}
+    return "The mode is " + mode[model] + System.lineSeparator();
+
+  }
+
+  private String effortStr(int size) {
+    double effort = table[mo][0] * Math.pow(size, table[mo][1]);
+    Cocomo.ef = effort;
+    return "Effort = " + dround(effort) + " Person-Month" + System.lineSeparator();
+
+  }
+
+  private String timeStr() {
+    double time = table[mo][2] * Math.pow(ef, table[mo][3]);
+    Cocomo.ti = time;
+    return "Development Time = " + dround(time) + " Months" + System.lineSeparator();
+  }
+
+  private String staffStr() {
+    double staff = ef / ti;
+    return "Average Staff Required = " + fround(staff) + " Persons" + System.lineSeparator();
+  }
+
+  public int fround(double x) {
+    return (int) Math.ceil(x);
+  }
+
+  public int dround(double y) {
+    return (int) (Math.round(y * 100000.00) / 100000.00);
+  }
 
 }
 
